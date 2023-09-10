89c8ad97c77443f94bd6e31ffe23e71ae9affc5c
hima707ch <107808488+hima707ch@users.noreply.github.com>
2023-09-11 00:35:23 +0530

    Update userAction.js

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -69,7 +69,7 @@ export const register = (userData) => async (dispatch) => {
     console.log("userData", userData);
 
     const { data } = await axios.post(
-      `https://n8wktq-8080.csb.app/api/v1/register`,
+      `/api/v1/register`,
       userData,
       config
     );
@@ -144,7 +144,7 @@ export const updatePassword = (passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `https://n8wktq-8080.csb.app/api/v1/password/update`,
+      `/api/v1/password/update`,
       passwords,
       config
     );
@@ -166,7 +166,7 @@ export const forgotPassword = (email) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.post(
-      `https://n8wktq-8080.csb.app/api/v1/password/forgot`,
+      `/api/v1/password/forgot`,
       email,
       config
     );
@@ -188,7 +188,7 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `https://n8wktq-8080.csb.app/api/v1/password/reset/${token}`,
+      `/v1/password/reset/${token}`,
       passwords,
       config
     );
@@ -207,7 +207,7 @@ export const getAllUsers = () => async (dispatch) => {
   try {
     dispatch({ type: ALL_USERS_REQUEST });
     const { data } = await axios.get(
-      `https://n8wktq-8080.csb.app/api/v1/admin/users`
+      `/api/v1/admin/users`
     );
 
     dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
@@ -221,7 +221,7 @@ export const getUserDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: USER_DETAILS_REQUEST });
     const { data } = await axios.get(
-      `https://n8wktq-8080.csb.app/api/v1/admin/user/${id}`
+      `/api/v1/admin/user/${id}`
     );
 
     dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
@@ -238,7 +238,7 @@ export const updateUser = (id, userData) => async (dispatch) => {
     const config = { headers: { "Content-Type": "application/json" } };
 
     const { data } = await axios.put(
-      `https://n8wktq-8080.csb.app/api/v1/admin/user/${id}`,
+      `/api/v1/admin/user/${id}`,
       userData,
       config
     );
@@ -258,7 +258,7 @@ export const deleteUser = (id) => async (dispatch) => {
     dispatch({ type: DELETE_USER_REQUEST });
 
     const { data } = await axios.delete(
-      `https://n8wktq-8080.csb.app/api/v1/admin/user/${id}`
+      `/api/v1/admin/user/${id}`
     );
 
     dispatch({ type: DELETE_USER_SUCCESS, payload: data });
